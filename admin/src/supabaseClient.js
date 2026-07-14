6f2b3096271c74c70c676073f0e36bb47532234c
unknown <gcccc406@gmail.com>
2026-07-15 00:11:29 +0330

    chore: remove admin folder from client repository

--- admin/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://xzzyhfyazwohdqqbjiiy.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6enloZnlhendvaGRxcWJqaWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMDM1NTEsImV4cCI6MjA5ODY3OTU1MX0.8KPZxd060ps2pc3oeDzBA9UG3fdHj_lPjnLhq0Q5eaM';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
