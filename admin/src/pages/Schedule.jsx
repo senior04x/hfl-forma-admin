ea9c099e7471737df586a9ed8efaf8af5d15b892
unknown <gcccc406@gmail.com>
2026-07-22 13:04:13 +0500

    style: enlarge HFL, LLF, Joma logos and align 7x7 logo horizontally

--- admin/src/pages/Schedule.jsx
+++ admin/src/pages/Schedule.jsx
@@ -393,18 +393,18 @@ const Schedule = () => {
         >
           <div className="sch-export-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '0px', padding: '0' }}>
             <div style={{ display: 'flex', alignItems: 'center', gap: '3px' }}>
-              <img src="/hfl-logo-for-jadval.png" alt="HFL" style={{ height: '75px', objectFit: 'contain' }} crossOrigin="anonymous" />
+              <img src="/hfl-logo-for-jadval.png" alt="HFL" style={{ height: '100px', objectFit: 'contain' }} crossOrigin="anonymous" />
               {exportLeague === '7x7 liga' && (
                 <>
                   <img src="/x.png" crossOrigin="anonymous" style={{ height: '18px', objectFit: 'contain', opacity: 0.7 }} />
-                  <img src="/llf-logo.png" alt="LLF" style={{ height: '60px', objectFit: 'contain' }} crossOrigin="anonymous" />
+                  <img src="/llf-logo.png" alt="LLF" style={{ height: '80px', objectFit: 'contain' }} crossOrigin="anonymous" />
                 </>
               )}
             </div>
 
             <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               {exportLeague === '7x7 liga' ? (
-                <img src="/7x7-liga.png" alt="7x7 Liga" style={{ height: '110px', maxWidth: '380px', objectFit: 'contain', marginRight: '75px' }} crossOrigin="anonymous" />
+                <img src="/7x7-liga.png" alt="7x7 Liga" style={{ height: '110px', maxWidth: '380px', objectFit: 'contain', marginRight: '75px', marginTop: '10px' }} crossOrigin="anonymous" />
               ) : (
                 LEAGUE_LOGOS[exportLeague] && (
                   <img src={LEAGUE_LOGOS[exportLeague]} alt={exportLeague} style={{ height: '110px', maxWidth: '380px', objectFit: 'contain' }} crossOrigin="anonymous" />
@@ -412,7 +412,7 @@ const Schedule = () => {
               )}
             </div>
 
-            <img src="/joma.png" alt="Joma" style={{ height: '60px', filter: exportLeague !== '7x7 liga' ? 'brightness(0) invert(1)' : 'none', objectFit: 'contain' }} crossOrigin="anonymous" />
+            <img src="/joma.png" alt="Joma" style={{ height: '80px', filter: exportLeague !== '7x7 liga' ? 'brightness(0) invert(1)' : 'none', objectFit: 'contain' }} crossOrigin="anonymous" />
           </div>
 
           <div className="sch-export-body">
